60b582eed6b560f8573ad99999be5ef61ff65c81
Lucas Wundervald <wundervald_2@hotmail.com>
2018-10-10 11:22:53 -0300

    reset da porra toda

--- reactjs/Desafio02/src/components/Lista.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Lista = props => (
-  <ul> {props.list.map(item => <li key={item.id.toString()}>{item.name}</li>)} </ul>
-);
-export default Lista;
--- reactjs/Desafio02/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import './styles/style.scss';
-
-import App from './components/App';
-
-render(<App />, document.getElementById('App'));
